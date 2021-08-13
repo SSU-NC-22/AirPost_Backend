bc6fcb181c9d69e1d9efff0ab70ffa207cf712de
eunseo <eunseo.choi.d@gmail.com>
2021-08-13 22:50:21 +0900

    Modify delivery struct

--- application/domain/model/delivery.go
+++ application/domain/model/delivery.go
@@ -1,12 +1,13 @@
 package model
 
 type Delivery struct {
-	ID   int    `json:"id" gorm:"primaryKey"`
-	SrcName string `json:"src_name" gorm:"type:varchar(32);not null"`
-	SrcPhone string `json:"src_phone" gorm:"type:varchar(32);not null"`
-	DestName string `json:"dest_name" gorm:"type:varchar(32);not null"`
-	DestPhone string `json:"dest_phone" gorm:"type:varchar(32);not null"`
-	// DestTag	int
+	ID			int		`json:"id" gorm:"primaryKey"`
+	SrcName		string	`json:"src_name" gorm:"type:varchar(32);not null"`
+	SrcPhone	string	`json:"src_phone" gorm:"type:varchar(32);not null"`
+	DestName	string	`json:"dest_name" gorm:"type:varchar(32);not null"`
+	DestPhone	string	`json:"dest_phone" gorm:"type:varchar(32);not null"`
+	NodeID		int		`json:"node_id" gorm:"not null"`
+	Node		Node	`json:"node" gorm:"foreignKey:NodeID"`
 }
 
 func (Delivery) TableName() string {
