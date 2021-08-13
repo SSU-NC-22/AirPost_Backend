84a3cb6e752359e14ea45ab12fbce14459d012f6
eunseo <eunseo.choi.d@gmail.com>
2021-08-13 19:40:53 +0900

    Add comments

--- application/domain/model/sensor.go
+++ application/domain/model/sensor.go
@@ -1,17 +1,6 @@
 package model
 
-// type Sensor struct {
-// 	ID           int           `json:"id" gorm:"primaryKey"`
-// 	Name         string        `json:"name" gorm:"type:varchar(32);unique;not null"`
-// 	SensorValues []SensorValue `json:"sensor_values" gorm:"foreignKey:SensorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-// 	Nodes        []Node        `json:"nodes" gorm:"many2many:has_sensors;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-// 	Logics       []Logic       `json:"logics" gorm:"foreignKey:SensorID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
-// }
-
-// func (Sensor) TableName() string {
-// 	return "sensors"
-// }
-
+// modified
 type SensorValue struct {
 	// SensorID  int    `json:"sensor_id" gorm:"primaryKey"`
 	NodeID		int		`json:"node_id" gorm:"primaryKey"`
