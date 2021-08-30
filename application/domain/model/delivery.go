8a385029e587c5b848ff237919ac5e3bc78dc772
eunseo <eunseo.choi.d@gmail.com>
2021-08-30 17:04:22 +0900

    Modify delivery model

--- application/domain/model/delivery.go
+++ application/domain/model/delivery.go
@@ -3,20 +3,20 @@ package model
 import "time"
 
 type Delivery struct {
-	ID			int		`json:"id" gorm:"primaryKey"`
-	OrderNum	string	`json:"order_num" gorm:"type:varchar(32);not null"`
+	ID			  int		`json:"id" gorm:"primaryKey"`
+	OrderNum	  string	`json:"order_num" gorm:"type:varchar(32);not null"`
+	DroneID		  int		`json:"drone_id" gorm:"not null"`
+	Drone		  Node		`json:"drone_node" gorm:"foreignKey:DroneID"`
 
-	SrcName		string	`json:"src_name" gorm:"type:varchar(32);not null"`
-	SrcPhone	string	`json:"src_phone" gorm:"type:varchar(32);not null"`
-	SrcNodeID	int		`json:"src_node_id" gorm:"not null"`
-	SrcNode		Node	`json:"src_node" gorm:"foreignKey:SrcNodeID"`
+	SrcName		  string	`json:"src_name" gorm:"type:varchar(32);not null"`
+	SrcPhone	  string	`json:"src_phone" gorm:"type:varchar(32);not null"`
+	SrcStationID  int		`json:"src_station_id" gorm:"not null"`
 
-	DestName	string	`json:"dest_name" gorm:"type:varchar(32);not null"`
-	DestPhone	string	`json:"dest_phone" gorm:"type:varchar(32);not null"`
-	DestNodeID	int		`json:"dest_node_id" gorm:"not null"`
-	DestNode	Node	`json:"dest_node" gorm:"foreignKey:DestNodeID"`
+	DestName	  string	`json:"dest_name" gorm:"type:varchar(32);not null"`
+	DestPhone	  string	`json:"dest_phone" gorm:"type:varchar(32);not null"`
+	DestStationID int		`json:"dest_station_id" gorm:"not null"`
 	
-	CreatedAt	time.Time `json:"created_at" gorm:"not null"`
+	CreatedAt	  time.Time `json:"created_at" gorm:"not null"`
 }
 
 func (Delivery) TableName() string {
